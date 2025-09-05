6d4b4316acb6ce83676dee658901fecd0649c156
no_carrier <myacobs@gmail.com>
2025-09-05 20:43:02 -0300

    Debugging blinker

--- avr/blink.c
+++ avr/blink.c
@@ -1,20 +1,22 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+
 int main (void) {
-    const int msDelay = 50;
+    static const uint16_t MS_DELAY = 1000;
 
     // Set PortB pin 4 direction to output
-    DDRB = 1<<DDB4;
+    DDRB |= (1 << DDB4);
 
-    PORTB = 0;
+    // Set to low
+    PORTB &= ~(1 << PB4);
 
     while (1) {
         // XOR pin 4
-        PORTB ^= 1<<PB4;
+        PORTB ^= (1<<PB4);
 
         // wait
-        _delay_ms(msDelay);
+        _delay_ms(MS_DELAY);
     }
 
     return 0;
