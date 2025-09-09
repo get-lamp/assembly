b3806bdbcf074570e561d0e4e7bd843eb3930576
no_carrier <myacobs@gmail.com>
2025-09-09 16:24:51 -0300

    Added OVF anc CTC timers examples

---
+++ avr/gcc/blink.c
@@ -0,0 +1,23 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main (void) {
+    static const uint16_t MS_DELAY = 500;
+
+    // Set PortB pin 4 direction to output
+    DDRB |= (1 << DDB4);
+
+    // Set to low
+    PORTB &= ~(1 << PB4);
+
+    while (1) {
+        // XOR pin 4
+        PORTB ^= (1<<PB4);
+
+        // wait
+        _delay_ms(MS_DELAY);
+    }
+
+    return 0;
+}
